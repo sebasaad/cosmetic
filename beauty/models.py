8f0efb9799d9c97c40aadf1f96e94053b1535a38
sebasaad <seba.saad115@gmail.com>
2022-10-07 01:07:16 -0700

    solve bugs

--- beauty/models.py
+++ beauty/models.py
@@ -1,13 +1,11 @@
 
 
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class User(models.Model):
-    userName = models.CharField(max_length=40)
-    email = models.CharField(max_length=40)
-    password = models.CharField(max_length=40)
+class User(AbstractUser):
+    pass
 
 
 class Category(models.Model):
